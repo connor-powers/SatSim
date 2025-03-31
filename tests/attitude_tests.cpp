752512497311fec7e58a40b80f87fc5f50882968
Connor Powers <20464743+connor-powers@users.noreply.github.com>
2025-03-31 15:55:46 -0700

    Running clang-format on new attitude test files
    Apparently no changes needed to JSON

--- tests/attitude_tests.cpp
+++ tests/attitude_tests.cpp
@@ -8,12 +8,12 @@
 const double tolerance = pow(10.0, -12);
 const double epsilon = pow(10.0, -11);
 
-
 TEST(AttitudeTests, PassivePitchTest1) {
   // Without any external or initial torques, the satellite's pitch angle
   // w.r.t. the LVLH frame should progress 2*pi radians over one full orbit
   Satellite test_satellite("../tests/attitude_test_input_1.json");
-  const double initial_true_anomaly = test_satellite.get_orbital_element("True Anomaly");
+  const double initial_true_anomaly =
+      test_satellite.get_orbital_element("True Anomaly");
   const double initial_pitch = test_satellite.get_attitude_val("Pitch");
   const double orbital_period = test_satellite.calculate_orbital_period();
   double test_timestep = 0.1;  // s
@@ -29,20 +29,24 @@ TEST(AttitudeTests, PassivePitchTest1) {
   }
   double evolved_pitch = test_satellite.get_attitude_val("Pitch");
   double after_loop_time = test_satellite.get_instantaneous_time();
-  EXPECT_TRUE(abs(evolved_pitch - initial_pitch) <= abs(after_loop_time - orbital_period))
-      << "Pitch didn't progress 2*pi radians over one orbit as expected. Difference: "
-      << evolved_pitch - initial_pitch << ", whereas the difference between the initial "
-      " and after-loop true anomalies was " << after_loop_time - orbital_period
-      << "\n";
+  EXPECT_TRUE(abs(evolved_pitch - initial_pitch) <=
+              abs(after_loop_time - orbital_period))
+      << "Pitch didn't progress 2*pi radians over one orbit as expected. "
+         "Difference: "
+      << evolved_pitch - initial_pitch
+      << ", whereas the difference between the initial "
+         " and after-loop true anomalies was "
+      << after_loop_time - orbital_period << "\n";
 }
 
 TEST(AttitudeTests, PassivePitchTest2) {
   // Let's make sure the pitch is progressing in the expected direction
   Satellite test_satellite("../tests/attitude_test_input_1.json");
-  const double initial_true_anomaly = test_satellite.get_orbital_element("True Anomaly");
+  const double initial_true_anomaly =
+      test_satellite.get_orbital_element("True Anomaly");
   const double initial_pitch = test_satellite.get_attitude_val("Pitch");
-  const double time_to_sim = 1; //s
-  double test_timestep = 1;  // s
+  const double time_to_sim = 1;  // s
+  double test_timestep = 1;      // s
   double current_time = test_satellite.get_instantaneous_time();
   double next_timestep = 0;
   while (current_time < time_to_sim) {
