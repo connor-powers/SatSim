b3aa008b7c7234086117ed645da0b3bb19fa5f77
Connor Powers <20464743+connor-powers@users.noreply.github.com>
2025-04-03 11:43:37 -0700

    Adding script to calculate code coverage from tests and moving simulation_setup to root directory

---
+++ test_coverage.sh
@@ -0,0 +1,8 @@
+cd build/
+cmake ..
+cmake --build .
+./elliptical_orbit_tests
+./circular_orbit_tests
+./attitude_tests
+./misc_tests
+gcovr -r .. --filter ../src/ --filter ../include/ --html-details output.html
